c34807a2e299229b803de1b99451139465f5a68c
Rimjhim Patidar <patidarsuman1984@gmail.com>
2025-12-23 15:54:44 +0530

    [Rimjhim] Added the program of strings of leading and trainling spaces

--- gcr-codebase/strings/Level-2/LeadingTrailingSpace.java
+++ gcr-codebase/strings/Level-2/LeadingTrailingSpace.java
@@ -1,53 +1,53 @@
-import java.util.*;
+		import java.util.*;
 
-public class LeadingTrailingSpace {
-	public static int[] findTrimIndexes(String text){
-		int start= 0,end = text.length() - 1;
-		while(start <=end && text.charAt(start)==' '){
-			start++;
-			
-		}
-		
-		while(end >= start && text.charAt(end) == ' '){
-			end--;
-		}
-		return new int[]{start ,end};
-	}
-	public static boolean createSubstring(String text, int start, int end){
-		String result = "";
-		for(int i =start ;i<end ; i++){
-			result+=text.charAt(i);
-		}
-		return result ;
-	}
-	public static String compareStrings(String s1, String s2){
-		if(s1.length() !=s2.length()){
-			return false;
-		}
-		for(int i = 0 ; i< s1.length ;i++){
-			if(s1[i] != s2[i]){
-				return false ;
+		public class LeadingTrailingSpace {
+			public static int[] findTrimIndexes(String text){
+				int start= 0,end = text.length() - 1;
+				while(start <=end && text.charAt(start)==' '){
+					start++;
+					
+				}
+				
+				while(end >= start && text.charAt(end) == ' '){
+					end--;
+				}
+				return new int[]{start ,end};
+			}
+			public static String createSubstring(String text, int start, int end){
+				String result = "";
+				for(int i =start ;i<end ; i++){
+					result+=text.charAt(i);
+				}
+				return result ;
+			}
+			public static boolean compareStrings(String s1, String s2){
+				if(s1.length() !=s2.length()){
+					return false;
+				}
+				for(int i = 0 ; i< s1.length() ;i++){
+					if(s1.charAt(i) != s2.charAt(i)){
+						return false ;
+					}
+				}
+				return true;
+			}
+
+
+			public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+
+				System.out.print("Enter text: ");
+				String text = sc.nextLine();
+
+				int[] indexes =findTrimIndexes(text);
+				String manualTrim = createSubstring(text ,indexes[0],indexes[1]);
+				String builtinTrim = text.trim();
+
+				// Compare results
+				boolean result = compareStrings(manualTrim, builtinTrim);
+
+				System.out.println("\nManual Trim Result : [" + manualTrim + "]");
+				System.out.println("Built-in Trim Result: [" + builtinTrim + "]");
+				System.out.println("Both results are equal: " + result);
 			}
 		}
-		return true;
-	}
-
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter text: ");
-        String text = sc.nextLine();
-
-        int[] indexes =findTrimIndexes(text);
-		String manualTrim = createSubstring(indexes[0],indexes[1]);
-		String builtinTrim = text.trim();
-
-        // Compare results
-        boolean result = compareStrings(manualTrim, builtinTrim);
-
-        System.out.println("\nManual Trim Result : [" + manualTrim + "]");
-        System.out.println("Built-in Trim Result: [" + builtinTrim + "]");
-        System.out.println("Both results are equal: " + result);
-    }
-}
