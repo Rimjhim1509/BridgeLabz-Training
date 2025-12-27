d4fa874092e8ffb1d30c6139d692851856400d02
Rimjhim Patidar <patidarsuman1984@gmail.com>
2025-12-27 11:58:43 +0530

    [Rimjhim] Added the OOPS program of class of palindrome checker

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/classandobject/level1/PalindromeChecker.java
@@ -0,0 +1,33 @@
+package com.classandobject.level1;
+
+public class PalindromeChecker {
+	String text;
+	PalindromeChecker(String text){
+		this.text = text;
+	}
+	public boolean isPalindrome() {
+		String cleanText = text.replaceAll("\\s","").toLowerCase();
+		String reverseText = new StringBuilder().reverse().toString();
+		return cleanText.equals(reverseText);
+
+	}
+	public void diplayResult() {
+		if(isPalindrome()) {
+			System.out.println(text+" is palindrome");
+			}
+		else {
+			System.out.println(text+" is not Palindrome");
+		}
+	}
+	public static void main(String[] args) {
+		//creating object to check the text
+		 PalindromeChecker checker1 = new PalindromeChecker("A man a plan a canal Panama");
+		 checker1.diplayResult();
+		
+		 //creating object to check the text
+		 PalindromeChecker checker2 = new PalindromeChecker("Hello");
+		 checker2.diplayResult();
+	}
+}
+
+
