d2b7db768547397eb51829100a5cee138937802c
Rimjhim Patidar <patidarsuman1984@gmail.com>
2025-12-29 11:19:15 +0530

    [Rimjhim] Added the program of metro smart card

---
+++ scenario-based-codebase/MetroSmartCard.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MetroSmartCard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter initial smart card balance: ");
+        int balance = sc.nextInt();
+
+        while (balance > 0) {
+            System.out.print("Enter distance  ");
+            int distance = sc.nextInt();
+
+            if (distance == -1) {
+                System.out.println("Journey ended by user.");
+                break;
+            }
+
+            int fare = (distance <= 5) ? 10 : 20;
+
+            if (balance >= fare) {
+                balance -= fare;
+                System.out.println("Fare deducted: ₹" + fare);
+                System.out.println("Remaining balance: ₹" + balance);
+            } else {
+                System.out.println("Insufficient balance!");
+                break;
+            }
+        }
+        sc.close();
+    }
+}
