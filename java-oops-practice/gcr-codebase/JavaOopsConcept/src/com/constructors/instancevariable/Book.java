63e57888500659e33eca4a8d34813d9567fcb392
Rimjhim Patidar <patidarsuman1984@gmail.com>
2025-12-31 10:45:55 +0530

    [Rimjhim] Added the program of Book Details

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructors/instancevariable/Book.java
@@ -0,0 +1,31 @@
+package com.constructors.instancevariable;
+
+class Book {
+    public String ISBN;
+    protected String title;
+    private String author;
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public static void main(String[] args) {
+		EBook pg = new EBook();
+		pg.ISBN  = "Secret" ;
+		pg.title = "Rimjhim";
+		pg.setAuthor("Patidar");
+		pg.display();
+		System.out.println("Author: " + pg.getAuthor());
+				
+	}
+}
+
+class EBook extends Book {
+    void display() {
+        System.out.println("ISBN: " + ISBN);
+        System.out.println("Title: " + title);
+    }
+}
