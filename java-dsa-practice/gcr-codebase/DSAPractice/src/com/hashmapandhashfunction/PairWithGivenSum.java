43b87e67fa635fcc79092ec055952dd9981c2152
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-12 13:22:14 +0530

    [Rimjhim] : Added the program of pair with given sum

---
+++ java-dsa-practice/gcr-codebase/DSAPractice/src/com/hashmapandhashfunction/PairWithGivenSum.java
@@ -0,0 +1,32 @@
+package com.hashmapandhashfunction;
+
+import java.util.*;
+
+public class PairWithGivenSum {
+
+    public static boolean hasPair(int[] arr, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            int complement = target - num;
+
+            if (map.containsKey(complement)) {
+                System.out.println("Pair found: " + complement + " + " + num + " = " + target);
+                return true;
+            }
+
+            map.put(num, 1); 
+        }
+
+        System.out.println("No pair found with sum " + target);
+        return false;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        PairWithGivenSum.hasPair(arr, target);
+    }
+}
