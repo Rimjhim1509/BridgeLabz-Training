f3eded100a42b0ae741517c7e1e2e06317d14a10
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-13 11:59:16 +0530

    [Rimjhim] : Added the program of File Reader to count the words

---
+++ java-dsa-practice/gcr-codebase/DSAPractice/src/com/binaryandlinearsearch/filereader/WordCount.java
@@ -0,0 +1,27 @@
+package com.binaryandlinearsearch.filereader;
+
+
+import java.io.*;
+
+public class WordCount {
+    public static void main(String[] args) throws IOException  {
+
+        String target = "java";
+        int count = 0;
+
+        BufferedReader br = new BufferedReader(new FileReader("fileSample.txt"));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                if (word.equalsIgnoreCase(target)) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Occurrences of '" + target + "': " + count);
+        br.close();
+    }
+}
