e53f122a75a4898e3f2e1f96417221ab5f09ebc4
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-19 15:15:50 +0530

    [Rimjhim] Added the program of ZipZipMart – Daily Sales Summary Report (Merge Sort)

---
+++ java-dsa-practice/scenario-based-codebase/ZipZipMart/ZipZip.java
@@ -0,0 +1,75 @@
+package com.ZipZipMart;
+
+class ZipZip {
+
+    // Merge Sort function
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            // Sort left half
+            mergeSort(arr, left, mid);
+
+            // Sort right half
+            mergeSort(arr, mid + 1, right);
+
+            // Merge both halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    
+    public static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        // Merge sorted arrays
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {   // stability maintained
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] sales = {450, 1200, 300, 800};
+
+        mergeSort(sales, 0, sales.length - 1);
+
+        System.out.println("Sorted Sales Report:");
+        for (int s : sales) {
+            System.out.print(s + " ");
+        }
+    }
+}
