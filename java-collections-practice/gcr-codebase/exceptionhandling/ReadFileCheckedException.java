89f9000d3519c57add33197195edd0ba66280e63
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-26 22:47:04 +0530

    [Rimjhim] Added the program of  Checked Exception (Compile-time Exception)

---
+++ java-collections-practice/gcr-codebase/exceptionhandling/ReadFileCheckedException.java
@@ -0,0 +1,22 @@
+package com.exceptionhandling;
+
+import java.io.*;
+
+public class ReadFileCheckedException {
+
+    public static void main(String[] args) {
+
+        File file = new File("data.txt");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
