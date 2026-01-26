e2875001732c8c965cb867256ec5bb9ed2888345
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-26 18:19:19 +0530

    [Rimjhim] Added the program of Rotate Elements in a List

---
+++ java-collections-practice/gcr-codebase/collections/list/RotateList.java
@@ -0,0 +1,25 @@
+package com.collections.list;
+
+import java.util.*;
+
+public class RotateList {
+	public static List<Integer> rotate(List<Integer> list ,int k){
+		int n= list.size();
+		k=k%n;
+		List<Integer> result =new ArrayList<>();
+		
+		//from k to end;
+		for(int i=k;i<n;i++) {
+			result.add(list.get(i));
+		}
+		for(int i=0;i<k;i++) {
+			result.add(list.get(i));
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(10,20,30,40,50);
+				
+				System.out.print(rotate(list, 3));
+	}
+}
