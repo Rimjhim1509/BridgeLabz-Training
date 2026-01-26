a861e4aebb2b092ba1ce3b1acd1f57a7b5523e55
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-26 18:22:27 +0530

    [Rimjhim] Added the program of Find Frequency of Elements

---
+++ java-collections-practice/gcr-codebase/collections/map/WordFreqCounter.java
@@ -0,0 +1,24 @@
+package com.collections.map;
+
+import java.util.HashMap;
+
+
+public class WordFreqCounter {
+	public static void main(String[] args) {
+		
+		String str = "Hello world, hello java!";
+		
+		str = str.toLowerCase();
+	
+		String[] words = str.split("[^a-zA-Z]+");
+		
+		HashMap<String, Integer> mp = new HashMap<>();
+		
+		for (String word : words) {
+			if (word.length() == 0) continue;
+			mp.put(word, mp.getOrDefault(word, 0) + 1);
+		}
+		
+		System.out.println(mp);
+	}
+}
