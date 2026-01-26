ca8befbee4b9736a660dfc69a8e282b0746d8a74
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-26 23:07:36 +0530

    [Rimjhim] Added the program Nested try-catch Block

---
+++ java-collections-practice/gcr-codebase/exceptionhandling/NestedTryCatch.java
@@ -0,0 +1,38 @@
+package com.exceptionhandling;
+
+import java.util.Scanner;
+
+public class NestedTryCatch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.print("Enter index: ");
+        int index = sc.nextInt();
+
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+
+        try {
+           
+            int element = arr[index];
+
+            try {
+                
+                int result = element / divisor;
+                System.out.println("Result: " + result);
+
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        }
+
+        sc.close();
+    }
+}
