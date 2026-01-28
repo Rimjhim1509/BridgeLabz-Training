9f8b9cf2c3a7702e1148b255c62009827bc47807
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-29 04:29:13 +0530

    [Rimjhim] Added the program to invoke dynamic mehtod at runtime using reflection

---
+++ java-collections-practice/gcr-codebase/reflection/dynamicobjectcreation/Student.java
@@ -0,0 +1,13 @@
+package com.reflactions.dynamicobjectcreation;
+
+public class Student {
+    private String name;
+
+    public Student() {
+        this.name = "Aman";
+    }
+
+    public void display() {
+        System.out.println("Student Name: " + name);
+    }
+}
