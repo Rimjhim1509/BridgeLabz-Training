f29593ba670a4b7e8404fd953a73f8089ca34e37
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-29 05:23:00 +0530

    [Rimjhim] Added the program of Read a CSV File and Print Data

---
+++ java-iostreams-practice/gcr-codebase/javaiostreamspractice/src/com/csvdatahandling/ReadCSV/ReadCSV.java
@@ -0,0 +1,23 @@
+package com.csvdatahandling.ReadCSV;
+
+
+import java.io.*;
+
+public class ReadCSV {
+    public static void main(String[] args) throws Exception {
+    	try(BufferedReader reader = new BufferedReader(new FileReader("src/source/students.csv"))){
+    		String line;
+
+    		reader.readLine();
+
+    		while ((line = reader.readLine()) != null) {
+    			String[] student = line.split(",");
+
+    			System.out.println(student[0] + " " + student[1] + " " + student[2] + " " + student[3]);
+    		}
+    	}
+    	catch(IOException e) {
+    		System.out.println(e.getMessage());
+    	}
+    }
+}
