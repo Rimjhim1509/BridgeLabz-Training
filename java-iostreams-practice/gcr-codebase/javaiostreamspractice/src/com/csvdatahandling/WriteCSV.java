4c2d0a00c4cc57677c9f1842916b649527c36842
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-01-31 12:44:02 +0530

    [Rimjhim] Added the program to Write Data to a CSV File

---
+++ java-iostreams-practice/gcr-codebase/javaiostreamspractice/src/com/csvdatahandling/WriteCSV.java
@@ -0,0 +1,39 @@
+package com.csvdatahandling;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteCSV {
+
+    public static void main(String[] args) {
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("employees.csv"))) {
+
+            
+            bw.write("Id,Name,Department,Salary");
+            bw.newLine();
+
+            // Employee records
+            bw.write("0,name1,dept1,55000");
+            bw.newLine();
+
+            bw.write("1,name2,dept2,48000");
+            bw.newLine();
+
+            bw.write("2,name3,dept3,62000");
+            bw.newLine();
+
+            bw.write("3,name4,dept4,45000");
+            bw.newLine();
+
+            bw.write("4,name5,dept5,70000");
+            bw.newLine();
+
+            System.out.println("CSV file written successfully.");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
