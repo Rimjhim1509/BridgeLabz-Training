c5a997ed0dfc96081e500713b6357225c96aeddc
Rimjhim Patidar <patidarsuman1984@gmail.com>
2026-02-01 18:17:17 +0530

    [Rimjhim] Added the program of Hospital Patient ID Printing

---
+++ java8-programming-practice/gcr-codebase/java8programmingpractice/src/com/methodreference/PatientIdPrinting.java
@@ -0,0 +1,14 @@
+package com.methodreference;
+
+import java.util.*;
+
+public class PatientIdPrinting {
+
+    public static void main(String[] args) {
+
+        List<Integer> patientIds = Arrays.asList(101, 102, 103, 104);
+
+    
+        patientIds.forEach(System.out::println);
+    }
+}
